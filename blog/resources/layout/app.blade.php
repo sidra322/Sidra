8c616e8917c4072d942dff3eeb08261e568dbffb
Mohsin <mohsinali786@live.com>
2023-07-24 10:47:43 +0500

    home set

--- blog/resources/layout/app.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ @$where }}" prefix="og: http://ogp.me/ns#">
-    <head itemscope itemtype="https://schema.org/WebSite">
-
-    </head>
-    <body>
-    asdf
-    </body>
-</html>
